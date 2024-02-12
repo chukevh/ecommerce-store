f386e3af52870bd4cb7169a877a20a7555d210eb
Kevin Chu <chu.kevin.h@gmail.com>
2024-02-12 18:49:22 +1100

    Fixed genre filter buttons and style on shirt page

--- frontend/src/pages/Shirts.js
+++ frontend/src/pages/Shirts.js
@@ -6,7 +6,7 @@ export default function Shirts() {
     const [shirts, setShirts] = React.useState([])
     const [searchParams, setSearchParams] = useSearchParams()
 
-    const brandFilter = searchParams.get("brand")
+    const genreFilter = searchParams.get("genre")
 
     React.useEffect(() => {
         fetch("/api/t-shirts")
@@ -14,8 +14,8 @@ export default function Shirts() {
             .then(data => setShirts(data))
     }, []) 
 
-    const displayedShirts = brandFilter 
-        ? shirts.filter(shirt => shirt.brand.toLowerCase().includes(brandFilter.toLowerCase()))
+    const displayedShirts = genreFilter 
+        ? shirts.filter(shirt => shirt.genre.toLowerCase().includes(genreFilter.toLowerCase()))
         : shirts
     
     const shirtElements = displayedShirts.map(shirt => (
@@ -40,14 +40,22 @@ export default function Shirts() {
         <div className="shirt-main-content">
             <h1 >Explore our Shirt Collection</h1>
             <div className="shirt-filters-container">
-                {/* <Link to="?brand=Baldurs Gate" className="shirt-brand bg3">Baldurs Gate</Link>
-                <Link to="?brand=Nilliur" className="shirt-brand nilliur">Nilliur</Link>
-                <Link to="?brand=Stardew" className="shirt-brand stardew">Stardew</Link>
-                <Link to="." className="shirt-brand clear-filters">Clear filters</Link> */}
-                <button onClick={() => handleSearchParams("brand", "Baldurs Gate")} className="shirt-brand bg3">Baldurs Gate</button>
-                <button onClick={() => setSearchParams({ brand: "Nilliur"})} className="shirt-brand nilliur">Nilliur</button>
-                <button onClick={() => setSearchParams({ brand: "Stardew"})} className="shirt-brand stardew">Stardew</button>
-                <button onClick={() => setSearchParams({})} className="shirt-brand clear-filters">Clear filters</button> 
+                <button 
+                    onClick={() => handleSearchParams("genre", "games")} 
+                    className={`shirt-genre-button ${genreFilter === "games" ? "selected" : ""}`}
+                >Games</button>
+                <button 
+                    onClick={() => handleSearchParams("genre", "anime")} 
+                    className={`shirt-genre-button ${genreFilter === "anime" ? "selected" : ""}`}
+                >Anime</button>
+                <button 
+                    onClick={() => handleSearchParams("genre", "misc")} 
+                    className={`shirt-genre-button ${genreFilter === "misc" ? "selected" : ""}`}
+                >Misc</button>
+                <button 
+                    onClick={() => setSearchParams({})} 
+                    className="shirt-genre-button clear-filters-button"
+                >Clear filters</button> 
             </div>
             <section className="shirt-container">
                 {shirtElements}
